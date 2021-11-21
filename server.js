62bd8845d162f8e4f7c228a07e26581e59cd8fa4
Peter Stríž <petulak8@gmail.com>
2021-11-21 17:06:52 +0100

    feature(SB-105): create a simple server to send basic instructions to modules

---
+++ server.js
@@ -0,0 +1,84 @@
+const WebSocketServer = require('ws').WebSocketServer;
+const cors = require('cors');
+const express = require('express');
+
+const PORT = 8000;
+const WS_PORT = 8001;
+
+const server = express();
+
+// fix cors
+server.use(cors());
+
+// parse application/x-www-form-urlencoded
+server.use(express.urlencoded({ extended: false }));
+
+// parse application/json
+server.use(express.json());
+
+// serve GUI
+server.use(express.static('build'));
+
+// backend server start
+server.listen(PORT, () => {
+  console.log('HTTP Server is running on PORT:', PORT);
+});
+
+// WS start
+const wss = new WebSocketServer({ port: WS_PORT }, () => {
+  console.log('WS Server is running on PORT:', WS_PORT);
+});
+
+const clients = [];
+
+wss.on('connection', (ws) => {
+  const wsClient = ws;
+  console.log('Client connected');
+  clients.push(wsClient);
+
+  wsClient.on('message', (message) => {
+    const data = JSON.parse(message.toString());
+    wsClient.moduleId = data.isMaster ? 'master' : data.moduleId;
+    wsClient.isMaster = data.isMaster;
+    console.log('Sprava prijata cez websocket!');
+    console.log(data);
+
+    // send message to all children
+    if (wsClient.isMaster) {
+      clients.forEach((module) => {
+        if (module?.moduleId && module?.moduleId === data?.moduleId) {
+          module.send(JSON.stringify(data));
+        }
+      });
+    }
+  });
+
+  wsClient.send('WS has succesfully connected to server');
+
+  wsClient.isAlive = true;
+  wsClient.on('pong', () => {
+    wsClient.isAlive = true;
+  });
+});
+
+// keepalive for WS clients
+setInterval(() => {
+  clients.forEach((client) => {
+    const wsClient = client;
+
+    if (!wsClient.isAlive) {
+      console.log(
+        `Client "${wsClient.moduleId}" not alive, closing websocket!`
+      );
+
+      wsClient.terminate();
+      const index = clients.indexOf(wsClient);
+      if (index > -1) {
+        clients.splice(index, 1);
+      }
+      return;
+    }
+    wsClient.isAlive = false;
+    wsClient.ping();
+  });
+}, 10000);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const WS_URL = 'ws://localhost:8001';
+
+const instructions = [
+  {
+    moduleId: 1,
+    instruction: 'SET_MOTOR_SPEED',
+    param: 30, // 0 - 100
+    category: 'MOTOR',
+    device: 'MOTOR_1',
+  },
+  {
+    moduleId: 1,
+    instruction: 'SET_TEMPERATURE',
+    param: 27,
+    category: 'TEMPERATURE',
+    device: 'TEMP_1',
+  },
+  {
+    moduleId: 1,
+    instruction: 'SET_FREQ',
+    param: 100, // 0 - 500
+    category: 'MOTOR',
+    device: 'MOTOR_1',
+  },
+  {
+    moduleId: 1,
+    instruction: 'ACCEL_TIME', // time to accelerate up to target speed (seconds)
+    param: 3,
+    category: 'MOTOR',
+    device: 'MOTOR_1',
+  },
+  {
+    moduleId: 1,
+    instruction: 'DECEL_TIME', // time to decelerate (seconds)
+    param: 2,
+    category: 'MOTOR',
+    device: 'MOTOR_1',
+  },
+  {
+    moduleId: 1,
+    instruction: 'SET_TIMER', // value 0 menas no timer - endless operation until stopped manually (minutes)
+    param: 0,
+    category: 'MOTOR',
+    device: 'MOTOR_1',
+  },
+];
 
 function App() {
+  const [instruction, setInstruction] = React.useState(instructions[0]);
+  const [visible, setVisible] = React.useState(false);
+  const ws = React.useRef(null);
+
+  React.useEffect(() => {
+    ws.current = new WebSocket(WS_URL);
+    ws.current.onmessage = (e) => {
+      console.log(e.data);
+    };
+  }, []);
+
+  function sendInstruction() {
+    console.log(JSON.stringify(instruction, null, 4));
+
+    const data = { ...instruction, isMaster: true };
+    ws.current.send(JSON.stringify(data));
+    setVisible(true);
+    setTimeout(() => setVisible(false), 1300);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <div className="buttons" style={{ display: 'flex' }}>
+        {instructions.map((instr) => {
+          return (
+            <button
+              key={instr.instruction}
+              onClick={() => setInstruction(instr)}
+            >
+              {instr.instruction}
+            </button>
+          );
+        })}
+      </div>
+
+      <textarea
+        className="instruction"
+        rows="10"
+        cols="80"
+        id="TITLE"
+        value={JSON.stringify(instruction, null, 2)}
+        onChange={(e) => setInstruction(JSON.parse(e.target.value))}
+      />
+
+      <button onClick={() => sendInstruction()}>Send instruction</button>
+
+      <div
+        style={{
+          color: 'green',
+          fontWeight: 'bold',
+          transition: 'opacity 0.5s ease-in-out',
+          opacity: visible ? 1 : 0,
+        }}
+      >
+        Sent!
+      </div>
     </div>
   );
 }
